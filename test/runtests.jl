a2229b2a4a342659026fba8a36cd3ce5e4bbdba2
Curt Da Silva <curt.dasilva@gmail.com>
2023-06-04 13:27:35 -0700

    fix: Only run `Aqua.jl` tests on 1.9

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using HierarchicalLogging
-using Aqua
-Aqua.test_all(HierarchicalLogging)
+
+# Aqua is unhappy with Dictionaries.jl on Julia 1.6
+if VERSION ≥ v"1.9"
+    using Aqua
+    Aqua.test_all(HierarchicalLogging)
+end
 
 include("TestHierarchicalLogging.jl")
